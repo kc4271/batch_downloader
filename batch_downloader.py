9704a1ead50919125100aeae9e24ce154df3bb96
Yan Kong <kc4271@gmail.com>
2014-08-18 14:10:30 +0800

    First version

---
+++ batch_downloader.py
@@ -0,0 +1,179 @@
+#!/usr/bin/env python 
+import Queue
+import socket
+import signal
+import threading
+import sys
+import os
+import time
+import math
+#third-party library
+import requests
+import eventlet
+import threadpool
+
+#global variables
+global stop
+global log_queue
+
+TIMEOUT = 600
+FOLDER_FILE_QUANTITY = 50000
+
+def signal_handler(signal, frame):
+    print('You have pressed Ctrl+C, program will stop!')
+    global stop
+    stop = True
+
+class ThreadPool:
+    def __init__(self, thread_num_):
+        self.thread_num = thread_num
+        self.callable = None
+        self.arg_list = None
+
+    def make_request(self, callable_, arg_queue):
+        self.callable = callable_
+        self.arg_list = arg_queue
+
+    def run(self):
+        threads = [None for e in range(self.thread_num)]
+        task_id = 0
+        live = False
+        for idx in range(self.thread_num):
+            if isinstance(threads[idx], threading.Thread):
+                if threads[idx].isAlive():
+                    live = True
+                else:
+                    if task_id < len(self.arg_list):
+                        task = self.arg_list[task_id]
+                        ##todo
+                        threads[idx] = self.callable(*self.arg_list[task_id])
+            
+            if isinstance(threads[idx], type(None)):
+                pass
+
+            time.sleep(0.2)
+
+def worker(task_list, dirpath, log):
+    global stop
+    
+    for idx in xrange(len(task_list)):
+        if stop:
+            break
+        try:
+            item_id, url = task_list[idx]
+            filename = url[url.rfind('/') + 1:]
+            if not url:
+                break
+            fpath = dirpath + '/%05d/' % (item_id / FOLDER_FILE_QUANTITY) + filename
+            
+            if os.path.exists(fpath):
+                continue
+
+            start_time = int(time.time())
+            write_file = False
+            timeout = eventlet.Timeout(TIMEOUT)
+            try:
+                u = requests.get(url)
+                write_file = True
+            except:
+                pass
+            finally:
+                timeout.cancel()
+            end_time = int(time.time())
+            
+            if write_file:
+                with open(fpath, 'wb') as f:
+                    f.write(u.content)
+                print 'Finish %d, %s, %ds' % (item_id, url, end_time - start_time)
+            else:
+                print 'Error %d, %s, %ds' % (item_id, url, end_time - start_time)
+        except Exception, e:
+            msg = '%s\t%s\n' % (url, type(e))
+            log.put(msg)      
+
+def get_url_list(poolsize):
+    f_lst = open(sys.argv[1])
+    
+    url_lists = [[] for e in range(poolsize)]
+    idx = 0
+    count = 0
+    for l in f_lst:
+        l = l.strip()
+        if not l:
+            continue
+        url_lists[idx].append([count, l])
+        idx = (idx + 1) % poolsize
+        count += 1
+    f_lst.close()
+
+    return url_lists
+
+class Log_worker(threading.Thread):
+    def __init__(self, f_log, queue_log):
+        threading.Thread.__init__(self)
+        self.log = f_log
+        self.queue = queue_log
+        self.close = False
+
+    def run(self):
+        print 'log worker run.'
+        while True:
+            try:
+                item = self.queue.get(True, 1)
+                f_log.write(item)
+                self.queue.task_done()
+            except:
+                if self.close:
+                    break
+        print 'log worker exit.'
+
+    def stop_thread(self):
+        self.close = True
+
+
+if __name__ == '__main__':
+    signal.signal(signal.SIGINT, signal_handler)
+    #parameter
+    #socket.setdefaulttimeout(600)
+    eventlet.monkey_patch()
+    poolsize = 100
+
+    #init
+    stop = False
+    log_queue = Queue.Queue()
+    pool = threadpool.ThreadPool(poolsize)
+    
+
+    if len(sys.argv) >= 3:
+        urls = get_url_list(poolsize)
+        print 'Url list loaded.'
+
+        #start log 
+        f_log = open(sys.argv[2], 'a')
+        log = Log_worker(f_log, log_queue)
+        log.daemon = True
+        log.start()
+
+        #create tasks
+        global dirpath
+        dirpath = './data/' + os.path.basename(sys.argv[1])[:-4]
+        if not os.path.isdir(dirpath):
+            os.makedirs(dirpath)
+
+        file_quantity = sum([len(e) for e in urls])
+        for idx in range(int(math.ceil(1.0 * file_quantity / FOLDER_FILE_QUANTITY))):
+            if not os.path.exists(dirpath + '/%05d' % idx):
+                os.mkdir(dirpath + '/%05d' % idx)
+        
+        thread_requests = threadpool.makeRequests(worker, [([urls[e]], {'dirpath':dirpath, 'log':log_queue}) for e in xrange(len(urls))])
+        [pool.putRequest(req) for req in thread_requests]
+        
+        pool.wait()
+
+        log.stop_thread()
+        log.join()
+
+    else:
+        print 'Usage: batch_downloader.py XXXX.lst YYYY.log'
+
+
